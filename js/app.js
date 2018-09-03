c2d59d90ff6bcf2fa5fc09061f06a05ad7ed437b
Antoni Alston <sntonialston@gmail.com>
2018-09-03 02:46:28 +0100

    making some adjustments

--- js/app.js
+++ js/app.js
@@ -9,3 +9,19 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     scrollTop: $($.attr(this, 'href')).offset().top
   }, 500);
 });
+
+document.getElementById('contact').addEventListener('click', function () {
+  document.body.scrollIntoView(false);
+});
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $('.navbar-burger').click(function() {
+
+    // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
+    $('.navbar-burger').toggleClass('is-active');
+    $('.navbar-menu').toggleClass('is-active');
+
+  });
+});
